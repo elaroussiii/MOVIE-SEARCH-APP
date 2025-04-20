609f693d33310c5b4bcbb5342fd5d588ef154eae
Mehdi ZYANE <mehdizyane@Mehdis-MacBook-Pro.local>
2025-04-21 01:48:08 +0200

    Adds cards to movies popular list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ApiService } from '../services/api.service'; // Importe ton service
-import { Movie } from '../models/movie.model'; // Interface pour le typage
-import { SearchBarComponent } from '../search-bar/search-bar.component';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ApiService} from '../services/api.service'; // Importe ton service
+import {Movie} from '../models/movie.model'; // Interface pour le typage
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardTitle
+} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SearchBarComponent],
+  imports: [CommonModule, MatCardTitle, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatCardImage, MatButton],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -17,7 +25,8 @@ export class HomeComponent implements OnInit {
   isLoading = true;     // Pour afficher un loader
 
   // 2. Injection du service API
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) {
+  }
 
   // 3. Méthode appelée à l'initialisation
   ngOnInit() {
